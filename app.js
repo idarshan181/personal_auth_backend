d1b41e265727e8bc74d7270ce4bd9283aaae126b
Darshan Patel <ddpatel97@gmail.com>
2022-02-05 00:33:06 +0530

    Added: User Route

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const cors = require("cors");
 
 let app = express();
-
+const userRoutes = require("./routes/user");
 app.use(express.json());
 app.use(cors());
-
+app.use("/user", userRoutes);
 app.get("/test", (req, res, next) => {
   console.log("endpoint working");
   res.json({ message: "hooray! welcome to our api!" });
