7d8b3da700b5d85643d8a1c2685961c15ee52c62
Darshan Patel <ddpatel97@gmail.com>
2022-02-08 00:20:02 +0530

    Modified: Structured imports for better readability

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ require("./config/database").connect();
 const express = require("express");
 const cors = require("cors");
 
-let app = express();
 const userRoutes = require("./routes/user");
+
+let app = express();
+
 app.use(express.json());
 app.use(cors());
+
 app.use("/user", userRoutes);
+
 app.get("/test", (req, res, next) => {
   console.log("endpoint working");
   res.json({ message: "hooray! welcome to our api!" });
